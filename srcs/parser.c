e3374058115113e61b465c2947537bcb9d695468
Chloe Maroude <cmaroude@c4r2p6.42nice.fr>
2022-08-02 10:10:04 +0200

    debug du chevron en debut de ligne

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: cmaroude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 13:46:28 by cmaroude          #+#    #+#             */
-/*   Updated: 2022/08/01 16:49:19 by cmaroude         ###   ########.fr       */
+/*   Updated: 2022/08/02 10:06:23 by cmaroude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,17 @@ void	do_pipe(t_lst_token *first_cmd, t_lst_token *sec_cmd, t_fd_redir *fds)
 	if (!first_pid)
 	{
 		dup2(pipe_fd[1], 1);
-		ft_parser(first_cmd, fds);
+		parser_chevron(first_cmd, fds);
 		dup2(fds->base_stdout, 1);
 		close(pipe_fd[1]);
 		exit(0);
 	}
 	sec_pid = fork();
-	if (!sec_pid)
+	if (!sec_pid && first_pid != 0)
 	{
 		dup2(pipe_fd[0], 0);
 		dup2(fds->base_stdout, 1);
-		ft_parser(sec_cmd, fds);
+		parser_chevron(sec_cmd, fds);
 		dup2(fds->base_stdin, 0);
 		close(pipe_fd[0]);
 		exit(0);
@@ -48,7 +48,6 @@ void	do_pipe(t_lst_token *first_cmd, t_lst_token *sec_cmd, t_fd_redir *fds)
 
 int	verif_pipe(t_lst_token *start, t_lst_token *actual, t_fd_redir *fds)
 {
-//	dprintf(2, "entering %s\n", __FUNCTION__);//debug
 	t_lst_token	*tmp_start;
 	t_lst_token	*tmp_actual;
 
@@ -59,6 +58,7 @@ int	verif_pipe(t_lst_token *start, t_lst_token *actual, t_fd_redir *fds)
 	if (ft_strcmp(actual->content, "|") == 0)
 	{	
 		ft_break(tmp_start, &tmp_actual);
+		(void)fds;
 		do_pipe(tmp_start, tmp_actual, fds);
 		return (1);
 	}
@@ -74,7 +74,7 @@ int	is_chevron(char *str)
 	return (0);
 }
 
-t_lst_token	*aplie_chevron(t_lst_token *chevron_tok, t_lst_token *lst_start,
+t_lst_token	*aplie_chevron(t_lst_token *chevron_tok, t_lst_token **lst_start,
 	t_fd_redir *fds)
 {
 	char		*chevron;
@@ -83,21 +83,27 @@ t_lst_token	*aplie_chevron(t_lst_token *chevron_tok, t_lst_token *lst_start,
 
 	if (!chevron_tok->next)
 		error("redirection must be followed by a file to redirect");
-	previous = lst_start;
+	if (chevron_tok->content != (*lst_start)->content)
+	{
+		previous = *lst_start;
+		while (previous && previous->next != chevron_tok && previous != chevron_tok)
+			previous = previous->next;
+		previous->next = chevron_tok->next->next;
+		chevron_tok->next->next = NULL;
+	}
+	else
+	{
+		previous = (*lst_start)->next->next;
+		*lst_start = previous;
+	}
 	chevron = chevron_tok->content;
 	redirect_file = chevron_tok->next->content;
-	while (previous->next != chevron_tok)
-		previous = previous->next;
-	previous->next = chevron_tok->next->next;
-	chevron_tok->next->next = NULL;
 	do_redirect_chevron(chevron, redirect_file, fds);
-	destroy_lst(chevron_tok);
 	return (previous);
 }
 
 int	parser_chevron(t_lst_token *token, t_fd_redir *fds)
 {
-//	dprintf(2, "entering %s\n", __FUNCTION__);//debug
 	t_lst_token	*re_start;
 	char		**std_args;
 
@@ -116,7 +122,7 @@ int	parser_chevron(t_lst_token *token, t_fd_redir *fds)
 	while (token)
 	{
 		if (is_chevron(token->content))
-			token = aplie_chevron(token, re_start, fds);
+			token = aplie_chevron(token, &re_start, fds);
 		token = token->next;
 	}
 	std_args = ft_construct(re_start);
