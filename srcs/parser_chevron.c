39f1536e8c35c24fa3939933b313b8bbe89943f4
Chloe Maroude <cmaroude@student.42nice.fr>
2022-08-13 15:32:22 +0200

    modification des fichiers d'erreurs pour qu'il ne sortent pas du programme minishell et ne continue pas la commande entree

--- srcs/parser_chevron.c
+++ srcs/parser_chevron.c
@@ -6,7 +6,7 @@
 /*   By: cmaroude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 15:58:10 by cmaroude          #+#    #+#             */
-/*   Updated: 2022/08/12 17:32:18 by cmaroude         ###   ########.fr       */
+/*   Updated: 2022/08/13 15:21:23 by cmaroude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,12 @@ int	check_forbidden_ends(t_lst_token *token)
 	if (!ft_strcmp(last->content, "|"))
 		return (error("error pipe at end", 258));
 	if (is_chevron(last->content))
-		return (error("redirection cannot be done without a file dumbass.", 258));
+		return (error("redirection cannot be done without a file dumbass."
+				, 258));
 	return (check_repeting_specials(token));
 }
 
-void	do_redirect_chevron_in(char *chevron, char *file, t_fd_redir *fds)
+int	do_redirect_chevron_in(char *chevron, char *file, t_fd_redir *fds)
 {
 	close(fds->in);
 	if (!ft_strcmp(chevron, "<<"))
@@ -59,14 +60,15 @@ void	do_redirect_chevron_in(char *chevron, char *file, t_fd_redir *fds)
 	{
 		fds->in = open(file, O_RDONLY);
 		if (fds->in < 0)
-			open_error(file);
+			return (open_error(file, 1));
 		dup2(fds->in, 0);
 	}
 	else
-		tech_error("entered redir_chevron_in with wrong chevron");
+		return (tech_error("entered redir_chevron_in with wrong chevron", 258));
+	return (0);
 }
 
-void	do_redirect_chevron_out(char *chevron, char *file, t_fd_redir *fds)
+int	do_redirect_chevron_out(char *chevron, char *file, t_fd_redir *fds)
 {
 	close(fds->out);
 	if (!ft_strcmp(chevron, ">>"))
@@ -74,14 +76,16 @@ void	do_redirect_chevron_out(char *chevron, char *file, t_fd_redir *fds)
 	else if (!ft_strcmp(chevron, ">"))
 		fds->out = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fds->out < 0)
-		open_error(file);
+		return (open_error(file, 1));
 	dup2(fds->out, 1);
+	return (0);
 }
 
-void	do_redirect_chevron(char *chevron, char *file, t_fd_redir *fds)
+int	do_redirect_chevron(char *chevron, char *file, t_fd_redir *fds)
 {
 	if (chevron[0] == '<')
-		do_redirect_chevron_in(chevron, file, fds);
+		return (do_redirect_chevron_in(chevron, file, fds));
 	else if (chevron[0] == '>')
-		do_redirect_chevron_out(chevron, file, fds);
+		return (do_redirect_chevron_out(chevron, file, fds));
+	return (0);
 }
