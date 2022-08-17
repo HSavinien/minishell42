c4700e87476a7bc679f4de4d9952549dd5d976e5
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-08-17 14:49:37 +0200

    improved -n option for echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,11 +6,28 @@
 /*   By: cmaroude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 14:06:18 by cmaroude          #+#    #+#             */
-/*   Updated: 2022/08/17 14:16:05 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/08/17 14:48:46 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+//test if an argument is of the form -nnnnnnn... with at least one n.
+//return 1 if arg is of this form, 0 else
+int	is_option_no_nl(char *arg)
+{
+	int	i;
+
+	if (!arg || arg[0] != '-')
+		return (0);
+	i = 1;
+	while (arg[i])
+	{
+		if (arg[i ++] != 'n')
+			return (0);
+	}
+	return (1);
+}
 
 int	ft_echo(int argc, char **argv)
 {
@@ -19,7 +36,7 @@ int	ft_echo(int argc, char **argv)
 	if (argc == 1)
 		return (printf("\n"));
 	i = 1;
-	while (argv[i] && !ft_strncmp(argv[i], "-n", 2))
+	while (argv[i] && is_option_no_nl(argv[i]))
 			i++;
 	while (argv[i])
 	{
@@ -27,7 +44,7 @@ int	ft_echo(int argc, char **argv)
 		if (i <= argc - 1)
 			printf(" ");
 	}	
-	if (ft_strncmp(argv[1], "-n", 2))
+	if (!is_option_no_nl(argv[1]))
 		printf("\n");
 	return (0);
 }
